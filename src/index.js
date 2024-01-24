2ec01a2d1cce854e39a7799dc3197b4b01fd3b12
kovalenkoandri <kovalenkoandrii@gmail.com>
2024-01-24 23:45:11 +0200

    rm example comments

--- src/index.js
+++ src/index.js
@@ -86,28 +86,5 @@ export default {
 
 			return jsonResponse;
 
-		// let img;
-		// if (url) {
-		// 	url = new URL(url).toString(); // normalize
-		// 	img = await env.BROWSER_KV_DEMO.get(url, { type: 'arrayBuffer' });
-		// 	if (img === null) {
-		// 		const browser = await puppeteer.launch(env.MYBROWSER);
-		// 		console.log(browser);
-		// 		const page = await browser.newPage();
-		// 		await page.goto(url);
-		// 		img = await page.screenshot();
-		// 		await env.BROWSER_KV_DEMO.put(url, img, {
-		// 			expirationTtl: 60 * 60 * 24,
-		// 		});
-		// 		await browser.close();
-		// 	}
-		// 	return new Response(img, {
-		// 		headers: {
-		// 			'content-type': 'image/jpeg',
-		// 		},
-		// 	});
-		// } else {
-		// 	return new Response('Please add an ?url=https://example.com/ parameter');
-		// }
 	},
 };
